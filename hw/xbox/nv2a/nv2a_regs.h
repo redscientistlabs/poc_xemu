abaire/xemu

nv2a: Implement shadow samplers

hw/xbox/nv2a/nv2a_regs.h
@@ -1214,6 +1214,15 @@
 #   define NV097_SET_COMBINER_COLOR_OCW                       0x00001E40
 #   define NV097_SET_COMBINER_CONTROL                         0x00001E60
 #   define NV097_SET_SHADOW_ZSLOPE_THRESHOLD                  0x00001E68
+#   define NV097_SET_SHADOW_COMPARE_FUNC                      0x00001E6C
+#       define NV097_SET_SHADOW_COMPARE_FUNC_NEVER                0x00000000
+#       define NV097_SET_SHADOW_COMPARE_FUNC_GREATER              0x00000001
+#       define NV097_SET_SHADOW_COMPARE_FUNC_EQUAL                0x00000002
+#       define NV097_SET_SHADOW_COMPARE_FUNC_GEQUAL               0x00000003
+#       define NV097_SET_SHADOW_COMPARE_FUNC_LESS                 0x00000004
+#       define NV097_SET_SHADOW_COMPARE_FUNC_NOTEQUAL             0x00000005
+#       define NV097_SET_SHADOW_COMPARE_FUNC_LEQUAL               0x00000006
+#       define NV097_SET_SHADOW_COMPARE_FUNC_ALWAYS               0x00000007
 #   define NV097_SET_SHADER_STAGE_PROGRAM                     0x00001E70
 #   define NV097_SET_DOT_RGBMAPPING                           0X00001E74
 #   define NV097_SET_SHADER_OTHER_STAGE_INPUT                 0x00001E78

hw/xbox/nv2a/pgraph_methods.h
@@ -177,6 +177,7 @@ DEF_METHOD(NV097, SET_SHADER_CLIP_PLANE_MODE)
 DEF_METHOD_RANGE(NV097, SET_COMBINER_COLOR_OCW, 8)
 DEF_METHOD(NV097, SET_COMBINER_CONTROL)
 DEF_METHOD(NV097, SET_SHADOW_ZSLOPE_THRESHOLD)
+DEF_METHOD(NV097, SET_SHADOW_COMPARE_FUNC)
 DEF_METHOD(NV097, SET_SHADER_STAGE_PROGRAM)
 DEF_METHOD(NV097, SET_DOT_RGBMAPPING)
 DEF_METHOD(NV097, SET_SHADER_OTHER_STAGE_INPUT)

redscientistlabs/poc_xemu

hw/xbox/nv2a/nv2a_regs.h
@@ -1218,6 +1218,15 @@
 #   define NV097_SET_COMBINER_COLOR_OCW                       0x00001E40
 #   define NV097_SET_COMBINER_CONTROL                         0x00001E60
 #   define NV097_SET_SHADOW_ZSLOPE_THRESHOLD                  0x00001E68
+#   define NV097_SET_SHADOW_DEPTH_FUNC                        0x00001E6C
+#       define NV097_SET_SHADOW_DEPTH_FUNC_NEVER                0x00000000
+#       define NV097_SET_SHADOW_DEPTH_FUNC_LESS                 0x00000001
+#       define NV097_SET_SHADOW_DEPTH_FUNC_EQUAL                0x00000002
+#       define NV097_SET_SHADOW_DEPTH_FUNC_LEQUAL               0x00000003
+#       define NV097_SET_SHADOW_DEPTH_FUNC_GREATER              0x00000004
+#       define NV097_SET_SHADOW_DEPTH_FUNC_NOTEQUAL             0x00000005
+#       define NV097_SET_SHADOW_DEPTH_FUNC_GEQUAL               0x00000006
+#       define NV097_SET_SHADOW_DEPTH_FUNC_ALWAYS               0x00000007
 #   define NV097_SET_SHADER_STAGE_PROGRAM                     0x00001E70
 #   define NV097_SET_DOT_RGBMAPPING                           0X00001E74
 #   define NV097_SET_SHADER_OTHER_STAGE_INPUT                 0x00001E78

hw/xbox/nv2a/pgraph_methods.h
@@ -177,6 +177,7 @@ DEF_METHOD(NV097, SET_SHADER_CLIP_PLANE_MODE)
 DEF_METHOD_RANGE(NV097, SET_COMBINER_COLOR_OCW, 8)
 DEF_METHOD(NV097, SET_COMBINER_CONTROL)
 DEF_METHOD(NV097, SET_SHADOW_ZSLOPE_THRESHOLD)
+DEF_METHOD(NV097, SET_SHADOW_DEPTH_FUNC)
 DEF_METHOD(NV097, SET_SHADER_STAGE_PROGRAM)
 DEF_METHOD(NV097, SET_DOT_RGBMAPPING)
 DEF_METHOD(NV097, SET_SHADER_OTHER_STAGE_INPUT)